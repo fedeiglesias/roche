a811cdbf810a9373ca860f8e9123b85015c55217
Federico Iglesias <fede.iglesiasc@gmail.com>
2021-05-25 23:07:22 +0200

    🔓 allow cors for all origins

--- api/handler.js
+++ api/handler.js
@@ -6304,6 +6304,10 @@ module.exports.data = async (event, context) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(data),
   };
 };
