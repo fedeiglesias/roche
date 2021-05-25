2557bf19507cbab86ad544b85c09916511ecbce4
Federico Iglesias <fede.iglesiasc@gmail.com>
2021-05-25 23:07:51 +0200

    🧑🏼‍⚕️ set default practitioner ID

--- src/components/patients/finder/index.tsx
+++ src/components/patients/finder/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 import * as R from "ramda";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import Card from '../../../components/shared/Card';
-import TextInput from '../../../components/shared/TextInput';
-import Button from '../../../components/shared/Button';
-import Section from '../../../components/shared/Section';
-import { IPatientItem } from '../../../models';
-import InfoMessage, { ETypeMessage } from '../../shared/InfoMessage'
-
-
+import Card from "../../../components/shared/Card";
+import TextInput from "../../../components/shared/TextInput";
+import Button from "../../../components/shared/Button";
+import Section from "../../../components/shared/Section";
+import { IPatientItem } from "../../../models";
+import InfoMessage, { ETypeMessage } from "../../shared/InfoMessage";
 
 const SearchForm = styled.div`
   display: flex;
@@ -26,34 +24,39 @@ const SearchForm = styled.div`
   }
 `;
 
-class PatientFinder extends React.Component<PatientFinderProps, IPatientFinderOwnState> {
+class PatientFinder extends React.Component<
+  PatientFinderProps,
+  IPatientFinderOwnState
+> {
   constructor(props: PatientFinderProps) {
     super(props);
 
     this.state = {
-      query: '',
+      query: "2588ac7f57fd9b49",
       notification: {
         show: false,
         type: ETypeMessage.error,
-        message: ''
-      }
-    }
+        message: "",
+      },
+    };
   }
 
   selectPractitionerHandler() {
     //Check if patient exist
-    let exist = R.findIndex(R.propEq('practitionerId', this.state.query))(this.props.patients);
+    let exist = R.findIndex(R.propEq("practitionerId", this.state.query))(
+      this.props.patients
+    );
 
     // If patient exist
-    if(exist > -1) {
+    if (exist > -1) {
       this.props.selectPractitioner(this.state.query);
     } else {
-      this.setState({ 
+      this.setState({
         notification: {
           show: true,
           type: ETypeMessage.error,
-          message: 'El ID del Médico ingresado no existe.'
-        }
+          message: "El ID del Médico ingresado no existe.",
+        },
       });
     }
   }
@@ -66,31 +69,30 @@ class PatientFinder extends React.Component<PatientFinderProps, IPatientFinderOw
         </header>
         <Card>
           <p>Introduzca el ID del Médico</p>
-          {
-            this.state.notification.show &&
-            <InfoMessage 
+          {this.state.notification.show && (
+            <InfoMessage
               message={this.state.notification.message}
-              type={this.state.notification.type}>
-            </InfoMessage>
-          }
+              type={this.state.notification.type}
+            ></InfoMessage>
+          )}
           <SearchForm>
-            <TextInput 
-              type="text" 
-              value={ this.state.query }
-              onChange={ (e:React.ChangeEvent<HTMLInputElement>) => 
+            <TextInput
+              type="text"
+              value={this.state.query}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                 this.setState({
-                  query: e.target.value, 
+                  query: e.target.value,
                   notification: {
-                    ...this.state.notification, 
-                    show: false
-                  }
+                    ...this.state.notification,
+                    show: false,
+                  },
                 })
               }
-              placeholder="ID de Médico"/>
+              placeholder="ID de Médico"
+            />
 
             <div>
-              <Button 
-                onClick={ () => this.selectPractitionerHandler() }>
+              <Button onClick={() => this.selectPractitionerHandler()}>
                 Buscar
               </Button>
             </div>
@@ -104,12 +106,12 @@ class PatientFinder extends React.Component<PatientFinderProps, IPatientFinderOw
 export default PatientFinder;
 
 export interface IPatientFinderOwnState {
-  query: string,
+  query: string;
   notification: {
-    show: boolean,
-    type: ETypeMessage,
-    message: string
-  }
+    show: boolean;
+    type: ETypeMessage;
+    message: string;
+  };
 }
 
 export interface IPatientFinderStateProps {
@@ -120,4 +122,5 @@ export interface IPatientFinderDispatchProps {
   selectPractitioner: (id: string) => void;
 }
 
-type PatientFinderProps = IPatientFinderStateProps & IPatientFinderDispatchProps
+type PatientFinderProps = IPatientFinderStateProps &
+  IPatientFinderDispatchProps;
